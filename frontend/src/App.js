507655ab44d684cc136b28cecb62a54ed47f070c
Brenda Pereira <brendacamara@outlook.com>
2020-10-28 09:47:58 -0300

    components

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Header from './components/Header'
+
+function App(){
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default App;
