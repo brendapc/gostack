1546938fb10b1eacbf7f4b53691691e9dde52839
Brenda Pereira <brendacamara@outlook.com>
2020-10-29 16:16:09 -0300

    config eslnt routes typescript

--- primeiro-projeto/src/server.ts
+++ primeiro-projeto/src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'hello node app' })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('server started ✨')
