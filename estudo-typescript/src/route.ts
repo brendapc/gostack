184d0c1ef8e17592034b3fafa6eb5798c627d1bc
Brenda Pereira <brendacamara@outlook.com>
2020-10-29 13:51:53 -0300

    tipos 2

--- estudo-typescript/src/route.ts
+++ estudo-typescript/src/route.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express'
 
+import createUser from './services/createUser'
+
 export function helloWorld(req: Request, res: Response){
-    return res.json({ message: 'hello dude' })
+    const user = createUser({
+        email: 'brenda',
+        password: 'jdnjs',
+        techs: [
+            'react',
+            'node',
+            { title: 'front' , experience: 2 },
+        ]
+    })
+
 }
