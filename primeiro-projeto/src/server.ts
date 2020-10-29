75b43e4c8b2bddbca5f5d9c12806a498e48d2526
Brenda Pereira <brendacamara@outlook.com>
2020-10-29 14:36:01 -0300

    configs

---
+++ primeiro-projeto/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res)=>{
+    return res.json({ message: 'hello node app'})
+})
+
+app.listen(3333, ()=>{
+    console.log('server started ✨')
+})
