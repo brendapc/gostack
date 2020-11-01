b321fb5f27564b0cd28ea6ffad51e376507717bf
Brenda Pereira <brendacamara@outlook.com>
2020-11-01 19:21:26 -0300

    middleware de auth

--- primeiro-projeto/src/services/AuthenticateUserService.ts
+++ primeiro-projeto/src/services/AuthenticateUserService.ts
@@ -1,14 +1,21 @@
 import { getRepository } from "typeorm";
 import { compare, hash } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
 import User from "../models/User";
+import authConfig from '../config/auth'
 
 interface Request {
 	email: string;
 	password: string;
 }
+interface Response{
+	user: User;
+	token: string;
 
+}
 class AuthenticateUserService {
-		public async execute({ email, password}: Request ): Promise<{ user: User}>{
+		public async execute({ email, password}: Request ): Promise<Response>{
 			const usersRepository = getRepository(User)
 
 			const user = await usersRepository.findOne({ where: { email }})
@@ -23,8 +30,15 @@ class AuthenticateUserService {
 				throw new Error('Incorrect email/password combination')
 			}
 
+			const { secret, expiresIn } = authConfig.jwt
+			const token = sign({ }, secret, {
+				subject: user.id,
+				expiresIn
+			})
+
 			return {
-				user
+				user,
+				token
 			}
 		}
 }
