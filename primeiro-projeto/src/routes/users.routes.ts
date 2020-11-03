634d20c158190037a3b5367ef6122f66b3232e57
Brenda Pereira <brendacamara@outlook.com>
2020-11-03 20:30:10 -0300

    evitando avatares duplicados

--- primeiro-projeto/src/routes/users.routes.ts
+++ primeiro-projeto/src/routes/users.routes.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express'
+import { response, Router } from 'express'
 import multer from 'multer'
 import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import UpdateUserAvatarService from '../services/UpdateUserAvatar'
 
 const usersRouter = Router()
 const upload = multer(uploadConfig)
@@ -30,8 +31,20 @@ usersRouter.post('/', async (req, res)=> {
 })
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (req, res)=>{
-	console.log(req.file)
-	return res.json({ok: true})
+	try{
+		const UpdateUserAvatar = new UpdateUserAvatarService
+
+		const user = await UpdateUserAvatar.execute({
+			user_id: req.user.id,
+			avatarFileName: req.file.filename
+		})
+
+		delete user.password
+		
+		return res.json(user)
+	}catch(err){
+		return response.status(400).json({ error: err.message})
+	}
 })
 export default usersRouter
 
