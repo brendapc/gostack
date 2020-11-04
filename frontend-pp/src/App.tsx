abeb8be54bab7751a7dd784c160c532fe8527099
Brenda Pereira <brendacamara@outlook.com>
2020-11-04 15:48:59 -0300

    prettier setup

--- frontend-pp/src/App.tsx
+++ frontend-pp/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
