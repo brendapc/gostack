0212f170e6f097f091a0ccb315b4bd45ff648ad8
Brenda Pereira <brendacamara@outlook.com>
2020-11-05 11:56:21 -0300

    estilização com react styled componets

--- frontend-react/src/pages/Dashboard/index.tsx
+++ frontend-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
-
-import { Title } from './styles'
+import { FiChevronRight } from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg'
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>
+  return (
+    <>
+      <img src={logoImg} alt="logo github" />
+      <Title>Explore os repositórios do Github</Title>
+      <Form>
+          <input placeholder="digite um nome" />
+          <button type="submit">go</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/33674561?s=460&u=8f06299386b1578d99c81125fc1ceae504480a5c&v=4" alt="Brenda Pereria" />
+          <div>
+            <strong>Repositorio</strong>
+            <p>Descrição do repositorio espero que peguemos com api</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/33674561?s=460&u=8f06299386b1578d99c81125fc1ceae504480a5c&v=4" alt="Brenda Pereria" />
+          <div>
+            <strong>Repositorio</strong>
+            <p>Descrição do repositorio espero que peguemos com api</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/33674561?s=460&u=8f06299386b1578d99c81125fc1ceae504480a5c&v=4" alt="Brenda Pereria" />
+          <div>
+            <strong>Repositorio</strong>
+            <p>Descrição do repositorio espero que peguemos com api</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard
--- frontend-react/src/styles/global.ts
+++ frontend-react/src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
+import githubBackground from '../assets/background.svg'
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -8,7 +10,7 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
   body {
-    background-color: #F0F0F5;
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
   }
   body, input, button {
@@ -19,5 +21,8 @@ export default createGlobalStyle`
     max-width: 960px;
     margin: 0 auto;
     padding: 40px 20px;
+  }
+  button {
+    cursor: pointer;
   }
 `
