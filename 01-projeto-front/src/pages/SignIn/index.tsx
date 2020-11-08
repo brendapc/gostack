8b5b93178bb25d8723b5163e6ed0822977dd6e62
Brenda Pereira <brendacamara@outlook.com>
2020-11-08 08:43:45 -0300

    context for login

--- 01-projeto-front/src/pages/SignIn/index.tsx
+++ 01-projeto-front/src/pages/SignIn/index.tsx
@@ -9,16 +9,21 @@ import getValidationErrors from '../../utils/getValidationErrors'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
-import AuthContext from '../../context/AuthContext'
+import { AuthContext } from '../../context/AuthContext'
 
 import { Container, Content, Background } from './styles'
+
+interface SignInForData {
+    email: string;
+    password: string
+}
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null)
 
-    const auth = useContext(AuthContext)
-    console.log(auth)
-    const handleSubmit = useCallback(async (data: object) => {
+    const { signIn } = useContext(AuthContext)
+
+    const handleSubmit = useCallback(async (data: SignInForData) => {
         try{
             formRef.current?.setErrors({})
             const schema = Yup.object().shape({
@@ -28,12 +33,17 @@ const SignIn: React.FC = () => {
             await schema.validate(data, {
                 abortEarly: false
             })
+
+            signIn({
+                email: data.email,
+                password: data.password
+            })
         }catch(err){
             const errors =  getValidationErrors(err)
             
             formRef.current?.setErrors(errors)
         }
-    }, [])
+    }, [signIn])
     return (
 
         <Container>
